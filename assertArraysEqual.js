6bb5505a533f21c6d85d1311a2662e4382425939
vagrant <maham.16491@gmail.com>
2020-08-23 21:04:04 +0000

    refactored assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,7 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
+const eqArrays = require('./eqArrays');
 
-};
-
-const eqArrays = function(a, b) {
-  let value = true;
-  if (a.length !== b.length) {
-    value = false;
-  } else {
-    for (let i = 0; i < a.length; i++) {
-      if (a[i] !== b[i]) {
-        value = false;
-      } else {
-        value = true;
-      }
-    }
-  }
-  return value;
-};
-
+//
 const assertArraysEqual = function(actual, expected) {
   let result = "";
   result = eqArrays(actual, expected);
@@ -34,7 +12,5 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3, 4, 5], [1, 2, 3, 4, 5]);
-assertArraysEqual([2, 4, 6], [2, 4, 8]);
-assertArraysEqual([3, 5, 7], [3, 5, 7]);
+//
+module.exports = assertArraysEqual;
