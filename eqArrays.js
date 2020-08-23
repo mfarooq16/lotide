a2e4749820bdeebd956d0828d4a416ccdaffee69
vagrant <maham.16491@gmail.com>
2020-08-23 21:02:10 +0000

    refactored eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,5 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔✔ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
 
-};
 
 const eqArrays = function(a, b) {
   for (let i = 0; i < a.length; i++) {
@@ -21,11 +14,4 @@ const eqArrays = function(a, b) {
 };
 
 
-// TEST CODE
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+module.exports = eqArrays;
