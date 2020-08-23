7338bdce133d020f2c4071bc65aa60f4bbc43cb2
vagrant <maham.16491@gmail.com>
2020-08-23 21:45:18 +0000

    refactored with mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,30 +2,13 @@ const assert = require('chai').assert;
 const tail   = require('../tail');
 
 describe("#tail", () => {
-  it("returns ["Lighthouse", "Labs"] for ["Hello", "Lighthouse", "Labs"]", () => {
+  it("returns [Lighthouse, Labs] for [Hello, Lighthouse, Labs]", () => {
     assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("returns [maham, adil] for [anam, maham, adil]", () => {
+    assert.deepEqual(tail(["anam", "maham", "adil"]), ["maham", "adil"]);
   });
 
 
 });
-
-
-
-
-/*
-//
-const assertEqual = require('../assertEqual');
-const tail = require('../tail');
-
-//
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-
-const names = tail(["anam", "maham", "adil"]);
-assertEqual(names.length, 2); // ensure we get back two elements
-assertEqual(names[0], "maham");
-assertEqual(names[1], "adil");
-*/
